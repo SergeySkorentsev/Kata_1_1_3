750da49f0285d91634cb693370b13675395ccfa2
sergey.skorentsev <sergey.skorentsev@yandex.ru>
2022-07-13 15:34:53 +0300

    updated UserDaoHibernateImpl, Util, Main

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,127 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.hibernate.resource.transaction.spi.TransactionStatus;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
-
-    }
-
-
     @Override
     public void createUsersTable() {
-
+        if(Util.getSessionFactory() == null) {
+            Util.connectH();
+        }
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("create table if not exists kata.Users(Id bigint not null auto_increment primary key, \n" +
+                    "Name varchar(32) not null, \n" +
+                    "LastName varchar(64) not null, \n" +
+                    "Age tinyint not null);").executeUpdate();
+            session.getTransaction().commit();
+        } catch (NullPointerException | HibernateException e) {
+            if (Util.getSessionFactory().openSession().getTransaction().getStatus() == TransactionStatus.ACTIVE
+                    || Util.getSessionFactory().openSession().getTransaction().getStatus() == TransactionStatus.MARKED_ROLLBACK) {
+                Util.getSessionFactory().openSession().getTransaction().rollback();
+            }
+            System.out.println("ERROR: " + e.getMessage());
+        }
     }
-
     @Override
     public void dropUsersTable() {
-
+        if(Util.getSessionFactory() == null) {
+            Util.connectH();
+        }
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("drop table if exists kata.Users;").executeUpdate();
+            session.getTransaction().commit();
+        } catch (NullPointerException | HibernateException e) {
+            if (Util.getSessionFactory().openSession().getTransaction().getStatus() == TransactionStatus.ACTIVE
+                    || Util.getSessionFactory().openSession().getTransaction().getStatus() == TransactionStatus.MARKED_ROLLBACK) {
+                Util.getSessionFactory().openSession().getTransaction().rollback();
+            }
+            System.out.println("ERROR: " + e.getMessage());
+        }
     }
-
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        if(Util.getSessionFactory() == null) {
+            Util.connectH();
+        }
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            Query query = session.createSQLQuery("insert into kata.Users (Name, LastName, Age) values (?, ?, ?);");
+            query.setParameter(1, name);
+            query.setParameter(2, lastName);
+            query.setParameter(3, age);
+            query.executeUpdate();
+            session.getTransaction().commit();
+            System.out.println(String.format("User с именем – %s добавлен в базу данных", name));
+        } catch (NullPointerException | HibernateException e) {
+            if (Util.getSessionFactory().openSession().getTransaction().getStatus() == TransactionStatus.ACTIVE
+                    || Util.getSessionFactory().openSession().getTransaction().getStatus() == TransactionStatus.MARKED_ROLLBACK) {
+                Util.getSessionFactory().openSession().getTransaction().rollback();
+            }
+            System.out.println("ERROR: " + e.getMessage());
+        }
     }
-
     @Override
     public void removeUserById(long id) {
-
+        if(Util.getSessionFactory() == null) {
+            Util.connectH();
+        }
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            Query query = session.createSQLQuery("delete from kata.Users where Id = ?;");
+            query.setParameter(1, id);
+            query.executeUpdate();
+            session.getTransaction().commit();
+        } catch (NullPointerException | HibernateException e) {
+            if (Util.getSessionFactory().openSession().getTransaction().getStatus() == TransactionStatus.ACTIVE
+                    || Util.getSessionFactory().openSession().getTransaction().getStatus() == TransactionStatus.MARKED_ROLLBACK) {
+                Util.getSessionFactory().openSession().getTransaction().rollback();
+            }
+            System.out.println("ERROR: " + e.getMessage());
+        }
     }
-
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        if(Util.getSessionFactory() == null) {
+            Util.connectH();
+        }
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            userList = session.createSQLQuery("select * from kata.users;").addEntity(User.class).list();
+            session.getTransaction().commit();
+        } catch (ClassCastException e) {
+            if (Util.getSessionFactory().openSession().getTransaction().getStatus() == TransactionStatus.ACTIVE
+                    || Util.getSessionFactory().openSession().getTransaction().getStatus() == TransactionStatus.MARKED_ROLLBACK) {
+                Util.getSessionFactory().openSession().getTransaction().rollback();
+            }
+            System.out.println("ERROR: " + e.getMessage());
+        }
+        return userList;
     }
-
     @Override
     public void cleanUsersTable() {
-
+        if(Util.getSessionFactory() == null) {
+            Util.connectH();
+        }
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("truncate kata.Users;").executeUpdate();
+            session.getTransaction().commit();
+        } catch (NullPointerException | HibernateException e) {
+            if (Util.getSessionFactory().openSession().getTransaction().getStatus() == TransactionStatus.ACTIVE
+                    || Util.getSessionFactory().openSession().getTransaction().getStatus() == TransactionStatus.MARKED_ROLLBACK) {
+                Util.getSessionFactory().openSession().getTransaction().rollback();
+            }
+            System.out.println("ERROR: " + e.getMessage());
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,13 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class Util {
     private static Connection conn = null;
+    private static SessionFactory sessionFactory = null;
     public static void connect() {
         try {
-            Class.forName ("com.mysql.cj.jdbc.Driver");
+            //Class.forName ("com.mysql.cj.jdbc.Driver");
             String userName = "root";
             String password = "qwert12345";
             String url = "jdbc:mysql://localhost:3306/mysql";
@@ -29,6 +41,44 @@ public class Util {
                 conn.close ();
                 System.out.println ("Database connection terminated... ");
             } catch (Exception ex) {
+                System.out.println ("Error in connection termination!");
+            }
+        }
+    }
+
+    public static void connectH() {
+        try {
+            Configuration configuration = new Configuration();
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mysql");
+            settings.put(Environment.USER, "root");
+            settings.put(Environment.PASS, "qwert12345");
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+            configuration.setProperties(settings);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            Metadata metadata = new MetadataSources(serviceRegistry)
+                    .addAnnotatedClass(User.class)
+                    .getMetadataBuilder()
+                    .build();
+            sessionFactory = metadata.getSessionFactoryBuilder().build();
+        } catch (HibernateException e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void closeH() {
+        if(sessionFactory != null) {
+            try {
+                sessionFactory.close();
+                System.out.println ("Database Hibernate connection terminated... ");
+            } catch (Exception e) {
                 System.out.println ("Error in connection termination!");
             }
         }
